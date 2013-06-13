aa4e2ae28bb8d45a0ad3c19da1b3e22baec77830
Calin Don <calin.don@gmail.com>
2013-06-13 14:13:13 +0300

    test timeout exception

--- tests/test_erequests.py
+++ tests/test_erequests.py
@@ -13,10 +13,18 @@ import erequests
 
 HTTPBIN_URL = os.environ.get('HTTPBIN_URL', 'http://httpbin.org/')
 
+
 def httpbin(*suffix):
     """Returns url for HTTPBIN resource."""
     return HTTPBIN_URL + '/'.join(suffix)
 
+
+def mark_exception(request):
+    return request.exception.__class__
+
+
+def return_exception(request):
+    return request.exception
 
 N = 5
 URLS = [httpbin('get?p=%s' % i) for i in range(N)]
@@ -98,10 +106,38 @@ class GrequestsCase(unittest.TestCase):
         resp = erequests.map(reqs, size=n)
         self.assertLess((time.time() - t), n)
 
+    def test_timeout(self):
+        n = 5
+        reqs = [erequests.get(httpbin('delay/3'), timeout=(i+1),
+                              exception_handler=mark_exception)
+                for i in range(n)]
+
+        resp = erequests.map(reqs, size=n)
+        self.assertListEqual(resp[0:3], [erequests.Timeout,
+                                         erequests.Timeout,
+                                         erequests.Timeout],
+                             "First three requests should have timeout")
+
+    def test_itimeout(self):
+        # with a pool size of 2 and 5 request timing out at 1, 2, 3, 4, 5
+        # total time shoud be 7
+        # timedout are marked with t and ok are marked with o
+        # time:     |---|---|---|---|---|---|---|---|
+        # thread-1: |-t-|-----t-----|-------o-------|
+        # thread-2: |---t---|-----o-----|
+        n = 5
+        s = 2
+        t = time.time()
+        reqs = [erequests.get(httpbin('delay/3'), timeout=i+1,
+                              exception_handler=return_exception)
+                for i in range(n)]
+        resp = list(erequests.imap(reqs, size=s))
+        self.assertGreaterEqual(time.time() - t, 7)
+        self.assertLess(time.time() - t, 8)
+
     def get(self, url, **kwargs):
         return erequests.map([erequests.get(url, **kwargs)])[0]
 
 
 if __name__ == '__main__':
     unittest.main()
-
